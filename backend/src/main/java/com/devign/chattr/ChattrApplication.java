70740d0c157c176b1ed2e1866fab09f90fc83dbf
Sari Hammad <sari_hammad@hotmail.com>
2025-05-13 17:44:53 -0400

    feat: init Spring backend

---
+++ backend/src/main/java/com/devign/chattr/ChattrApplication.java
@@ -0,0 +1,13 @@
+package com.devign.chattr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devign/chattr/ChattrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devign.chattr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
