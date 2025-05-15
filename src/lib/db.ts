668a91d065c6c353dae078fcaaf5a82352e3374c
Sari Hammad <sari_hammad@hotmail.com>
2025-05-15 14:15:01 -0400

    refactor: update directory structure

--- src/lib/db.ts
+++
@@ -1,5 +0,0 @@
-// src/lib/db.ts
-// Prisma client connection
-import { PrismaClient } from '@prisma/client';
-
-export const db = new PrismaClient();
--- src/lib/stripe.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/stripe.ts
-import Stripe from "stripe";
-
-if (!process.env.STRIPE_SECRET_KEY) throw new Error("Missing Stripe key");
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
