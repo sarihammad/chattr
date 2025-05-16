ce61d0e608ff4ee2d8474ea296292f56e285a7fd
Sari Hammad <sari_hammad@hotmail.com>
2025-05-16 10:28:36 -0400

    feat: update User model fields

--- backend/src/main/java/com/devign/chattr/model/User.java
+++ backend/src/main/java/com/devign/chattr/model/User.java
@@ -1,15 +1,7 @@
 package com.devign.chattr.model;
 
 import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -34,6 +26,12 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private Boolean isOnline;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
